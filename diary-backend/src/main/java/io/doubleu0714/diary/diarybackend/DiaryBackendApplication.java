171de8acaaceb4f9e259ea26a739f5e95aa41bed
Wonhyung Park <w0714@Wonhyungui-MacBookPro.local>
2019-07-09 16:10:11 +0900

    init project

---
+++ diary-backend/src/main/java/io/doubleu0714/diary/diarybackend/DiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package io.doubleu0714.diary.diarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryBackendApplication.class, args);
+	}
+
+}
---
+++ diary-backend/src/test/java/io/doubleu0714/diary/diarybackend/DiaryBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package io.doubleu0714.diary.diarybackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiaryBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
